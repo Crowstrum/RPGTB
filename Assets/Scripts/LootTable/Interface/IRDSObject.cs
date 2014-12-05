120b211f627d71b4a0c2b7c73fa8e06c247c2f84
crowstrum <crowstrum@gmail.com>
2014-12-05 13:49:01 -0600

    Initial

---
+++ Assets/Scripts/LootTable/Interface/IRDSObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IRDSObject {
+
+    double rdsProbability { get; set; } //drop chance
+    bool rdsUnique { get; set; } //only drops once per query
+    bool rdsAlways { get; set; } // drops always
+    bool rdsEnabled { get; set; } // can it drop now?
+
+    event Eventhandler 
+
+}
---
+++ Assets/Scripts/LootTable/Interface/IRDSTable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public interface IRDSTable {
+    int rdsCount { get; set; } //how many items can drop from this table
+    IEnumerable<IRDSObject> rdsContents { get; } //the contents of this table
+    IEnumerable<IRDSObject> rdsResult { get; } // the result set 
+}
